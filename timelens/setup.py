7ac337fdb9fda9f3d8687f28deb7284f8fac25e0
Enrique <lopezyouhei@gmail.com>
2025-04-18 22:55:36 +0200

    update setup to ensure torch version is installed constraints text file ensures that the necessary torch versions are installed setup.py was updated with an installation order

--- timelens/setup.py
+++ timelens/setup.py
@@ -1,14 +1,35 @@
 import os
 import subprocess
+import sys
 
 base_path = os.path.dirname(__file__)
 
 # automaticall install required packages during setup
 def install_requirements() -> None:
-    backend_path = os.path.join(base_path, 'backend/requirements.txt')
-    frontend_path = os.path.join(base_path, 'frontend/requirements.txt')
-    subprocess.run(["pip", "install", "-r", backend_path], check=True)
-    subprocess.run(["pip", "install", "-r", frontend_path], check=True)
+    backend_path = os.path.join(base_path, 'backend')
+    frontend_path = os.path.join(base_path, 'frontend')
+
+    # install torch stack first
+    subprocess.run([
+        sys.executable, "-m", "pip", "install",
+        "torch==1.13.1+cu117",
+        "torchvision==0.14.1",
+        "torchaudio==0.13.1+cu117",
+        "--extra-index-url", "https://download.pytorch.org/whl/cu117"
+    ], check=True)
+
+    # install remaining backend 
+    subprocess.run([
+        sys.executable, "-m", "pip", "install",
+        "-r",os.path.join(backend_path, "requirements.txt"),
+        "--constraint", os.path.join(backend_path, "constraints.txt")
+    ], check=True)
+
+    # install frontend
+    subprocess.run([
+        sys.executable, "-m", "pip", "install",
+        "-r", os.path.join(frontend_path, "requirements.txt")
+    ], check=True)
 
 install_requirements()
 
