43312fda1656576bce31e0c56b8cf8a8d86dd7de
Enrique <lopezyouhei@gmail.com>
2025-04-18 20:15:17 +0200

    create sections for functionality and displaying examples

--- timelens/frontend/pages/1_Enhance.py
+++ timelens/frontend/pages/1_Enhance.py
@@ -4,32 +4,39 @@ import requests
 st.title("Image Enhancer")
 st.write("Upload an image and select a resolution (2x or 4x).")
 
-uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
-scale_option = st.radio("Select scale", ["x2", "x4"])
-button = st.button("Enhance Image")
+col1, col2 = st.columns([2, 1])
 
-if button and uploaded_file and scale_option:
-    with st.spinner("Enhancing..."):
-        files = {
-            "file": (uploaded_file.name, uploaded_file.getvalue(), uploaded_file.type)
-        }
-        data = {"scale": scale_option}
-        response = requests.post(
-            "http://localhost:8000/enhance", 
-            files=files,
-            data=data
-        )
+with col1:
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    scale_option = st.radio("Select scale", ["x2", "x4"])
+    button = st.button("Enhance Image")
 
-        if response.status_code == 200:
-            st.image(
-                response.content, 
-                caption="Enhanced Image", 
-                use_container_width=True
+    if button and uploaded_file and scale_option:
+        with st.spinner("Enhancing..."):
+            files = {
+                "file": (uploaded_file.name, uploaded_file.getvalue(), uploaded_file.type)
+            }
+            data = {"scale": scale_option}
+            response = requests.post(
+                "http://localhost:8000/enhance", 
+                files=files,
+                data=data
             )
-            st.download_button(
-                "Download Enhanced Image", 
-                response.content, 
-                "enhanced_image.png"
-            )
-        else:
-            print("Failed to enhance image.")
+
+            if response.status_code == 200:
+                st.image(
+                    response.content, 
+                    caption="Enhanced Image", 
+                    use_container_width=True
+                )
+                st.download_button(
+                    "Download Enhanced Image", 
+                    response.content, 
+                    "enhanced_image.png"
+                )
+            else:
+                print("Failed to enhance image.")
+
+with col2:
+    st.write("Enhance example images:")
+    
