b99c8c354bd7bcb0e3a43bd730c3b5fd9de5379a
Enrique <lopezyouhei@gmail.com>
2025-04-18 22:12:00 +0200

    update function name to be more universal

--- timelens/install_models.py
+++ timelens/install_models.py
@@ -3,7 +3,7 @@ import urllib.request
 
 from backend import MODEL_PATH
 
-def download_realesrgan(url: str, output_folder: str = MODEL_PATH) -> None:
+def download_model(url: str, output_folder: str = MODEL_PATH) -> None:
     os.makedirs(output_folder, exist_ok=True)
     filename = url.split("/")[-1]
     output_path = os.path.join(output_folder, filename)
@@ -12,9 +12,10 @@ def download_realesrgan(url: str, output_folder: str = MODEL_PATH) -> None:
         urllib.request.urlretrieve(url, output_path)
 
 if __name__ == "__main__":
-    model_urls = [
+    realesrgan_urls = [
         "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.1.0/RealESRGAN_x4plus.pth",
-        "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth"
+        "https://github.com/xinntao/Real-ESRGAN/releases/download/v0.2.1/RealESRGAN_x2plus.pth",
+        "https://huggingface.co/spensercai/DeOldify/resolve/main/ColorizeStable_gen.pth"
     ]
-    for model_url in model_urls:
-        download_realesrgan(model_url)
+    for model_url in realesrgan_urls:
+        download_model(model_url)
